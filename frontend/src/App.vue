4e111db556b6fb67ee4d125566acb9cdf700bffd
pytherik <erikberndt@gmx.net>
2023-10-22 13:23:48 +0200

    first request from frontend successful :-)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,85 +1,32 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
   <RouterView />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  height: 5rem;
+  background-color: rosybrown;
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  padding: 0 2rem;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+a {
+  font-size: 1.5rem;
+  text-decoration: none;
+  color: #dedeed;
+  margin: 0 2rem;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid black;
+  border-radius: 8px;
 }
 </style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { onMounted, ref } from 'vue'
+
+const hello = ref('')
+const fetchData = async () => {
+  try {
+    const res = await fetch('http://localhost:3000', { method: 'GET' })
+    hello.value = await res.text()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+onMounted(() => fetchData())
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>This is the Home Page</h1>
+    <h1>{{ hello }}</h1>
   </main>
 </template>
